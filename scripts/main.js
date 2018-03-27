4e08275268af93698feca9ba5b6ad9ba22b08da5
jaeyeoupjung <wp01053@naver.com>
2018-03-27 11:22:07 +0900

    JavaScript_2018.03.27

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,8 @@ var iceCream = 'chocolate';
 var id = prompt('ID 입력','');
 alert(id +'가 로그인함.');
 var passwordSystem = '12345'
-var pssword = prompt('PW 입력','')
-if (pssword ===paswordSystem)
+var password = prompt('PW 입력','')
+if (password ===passwordSystem)
 alert('정상로그인!');
 else alert('비밀번호 오류');
 
