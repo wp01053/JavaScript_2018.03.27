0792518b4d95c8bbdce577e71718dc73f0173956
jaeyeoupjung <wp01053@naver.com>
2018-03-27 10:55:31 +0900

    JavaScript_2018.03.27

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello OUTBACK!';
+var text = 10.5;
+var a = 33.7;
+var b = a*text;
+var iceCream = 'chocolate';
+var id = prompt('ID 입력','');
+
+var ps = prompt('password 입력 ', '');
+alert(id +'가 로그인함.');
+
+
+
