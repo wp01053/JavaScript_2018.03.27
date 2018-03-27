c46fddd73a1f990a92122365d9e6b322e43fe7d4
jaeyeoupjung <wp01053@naver.com>
2018-03-27 11:55:27 +0900

    JavaScript_2018.03.27 image

--- jae.bat
+++ jae.bat
@@ -1,3 +1,3 @@
 git add --all
-git commit -m "JavaScript_2018.03.27"
+git commit -m "JavaScript_2018.03.27 image"
 git push -u origin master
--- jae3.bat.txt
+++
@@ -1,3 +0,0 @@
-git add --all
-git commit -m "JavaScript_2018.03.27"
-git push -u origin master
