e57a4e6e47864f19c4777abb69de1bd8d9fdc066
jaeyeoupjung <wp01053@naver.com>
2018-03-27 11:15:45 +0900

    JavaScript_2018.03.27

---
+++ jae1.bat
@@ -0,0 +1,3 @@
+git add --all
+git commit -m"JavaScript_2018.03.27"
+git push -u origin master
--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,10 @@ var a = 33.7;
 var b = a*text;
 var iceCream = 'chocolate';
 var id = prompt('ID 입력','');
-
-var ps = prompt('password 입력 ', '');
 alert(id +'가 로그인함.');
-
-
+var passwordSystem = '12345'
+var pssword = prompt('PW 입력','')
+if (pssword ===paswordSystem)
+alert('정상로그인!');
+else alert('비밀번호 오류');
 
